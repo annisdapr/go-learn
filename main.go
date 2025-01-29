88d4c39931b0a39865e1d65d34b0674a19d74789
annisa <annissadwiaprilia@gmail.com>
2025-01-29 22:25:33 +0700

    test

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Hello world")
+	fmt.Println("Hello", "world!", "how", "are", "you")
+
+	var firstName string = "john"
+	var age int = 23
+    fmt.Printf("halo %s %d!\n", firstName, age)
+}
