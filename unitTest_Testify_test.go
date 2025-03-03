4c802b5f703290e5a130ec8fa1210a278fd6525b
annisa <annissadwiaprilia@gmail.com>
2025-03-03 13:45:53 +0700

    basic-2

---
+++ unitTest_Testify_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+    "testing"
+    "github.com/stretchr/testify/assert"
+)
+
+// Data untuk pengujian
+var (
+    kubus              Kubus   = Kubus{4}
+    volumeSeharusnya   float64 = 64
+    luasSeharusnya     float64 = 96
+    kelilingSeharusnya float64 = 48
+)
+
+// Pengujian dengan Testify
+func TestHitungVolume(t *testing.T) {
+    assert.Equal(t, volumeSeharusnya, kubus.Volume(), "perhitungan volume salah")
+}
+
+func TestHitungLuas(t *testing.T) {
+    assert.Equal(t, luasSeharusnya, kubus.Luas(), "perhitungan luas salah")
+}
+
+func TestHitungKeliling(t *testing.T) {
+    assert.Equal(t, kelilingSeharusnya, kubus.Keliling(), "perhitungan keliling salah")
+}
