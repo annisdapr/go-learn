d652532c30f9d2eacc90035b3407d8bb9ede9458
annisa <annissadwiaprilia@gmail.com>
2025-03-05 22:49:43 +0700

    basic-3

--- B5RenderPartial/parseFile.go
+++ B5RenderPartial/parseFile.go
@@ -9,23 +9,31 @@ import (
 type M map[string]interface{}
 
 func main() {
-	var tmpl, err = template.ParseGlob("views/*")
-	if err != nil {
-		panic(err.Error())
-		return
-	}
 
-	http.HandleFunc("/index", func(w http.ResponseWriter, r *http.Request) {
-		var data = M{"name": "Batman"}
-		err = tmpl.ExecuteTemplate(w, "index", data)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
+	http.HandleFunc("/index",
+		func(w http.ResponseWriter, r *http.Request) {
+			var data = M{"name": "Batman"}
+			var tmpl = template.Must(template.ParseFiles(
+				"views/index.html",
+				"views/_header.html",
+				"views/_message.html",
+			))
+	
+			var err = tmpl.ExecuteTemplate(w, "index", data)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			}
 	})
 
 	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
 		var data = M{"name": "Batman"}
-		err = tmpl.ExecuteTemplate(w, "about", data)
+		var tmpl = template.Must(template.ParseFiles(
+			"views/about.html",
+			"views/_header.html",
+			"views/_message.html",
+		))
+   
+		var err = tmpl.ExecuteTemplate(w, "about", data)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
